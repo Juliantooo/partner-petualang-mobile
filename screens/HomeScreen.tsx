519fd4eb4cfa0aa2fb1315167df65752f5933412
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-13 16:45:03 +0700

    update home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,32 +1,51 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import Banner from '../components/Banner';
+import { Box, Flex, ScrollView, VStack } from 'native-base';
+import useSellItems from '../hooks/useSellItems';
+import CardItem from '../components/CardItem';
+import SkeletonCard from '../components/Skeleton';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
+
+  const { isLoading, items } = useSellItems();
+  const skeletonNum = Array(6).fill(null).map((_, i) => i);
+
+  const handlePressItemCard = (id) => {
+    navigation.navigate('DetailItem', { id })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/HomeScreen.tsx" />
+    <View style={{ backgroundColor: '#fff' }} >
+      <ScrollView>
+        <VStack space='4'>
+          <Box>
+            <Banner />
+          </Box>
+          <Flex flexDirection='row' flexWrap='wrap' justifyContent='space-around' px='4' >
+            {
+              isLoading ?
+                skeletonNum.map((_, idx) => (
+                  <SkeletonCard key={idx} />
+                ))
+                :
+                items.map((item: any) => (
+                  <CardItem
+                    key={item.id}
+                    image={item.image}
+                    name={item.name}
+                    price={item.price}
+                    discount={item.discount}
+                    category={item.category}
+                    rented={item.rented}
+                    id={item.id}
+                    handlePressItemCard={handlePressItemCard}
+                  />
+                ))
+            }
+          </Flex>
+        </VStack>
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
