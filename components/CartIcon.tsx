76833e9a11de260ecca0d32bb7f957b071456858
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-13 16:19:28 +0700

    add cart Icon

---
+++ components/CartIcon.tsx
@@ -0,0 +1,47 @@
+import { Badge, Box, Pressable } from "native-base"
+import { Ionicons } from '@expo/vector-icons';
+import useCartItems from "../hooks/useCartItems";
+import useAuth from "../hooks/useAuth";
+import { ROUTES_NAME } from "../libs/router";
+import notification from "../libs/notification";
+
+const CartSection = ({ navigation }: any) => {
+
+    const { isLogin } = useAuth();
+    const { cartItemsCount } = useCartItems();
+
+    const handlePressGoToCart = () => {
+        if (!isLogin) {
+            notification.error('Anda belum masuk ke akun anda!');
+            return navigation.navigate(ROUTES_NAME.LOGIN);
+        }
+        navigation.navigate('Cart')
+    }
+
+    return (
+        <Box marginRight='26'>
+            {
+                cartItemsCount ?
+                    <Badge
+                        colorScheme="danger" rounded='lg' mb={-4} mr={-3} zIndex={1} variant="solid" alignSelf="flex-end" _text={{
+                            fontSize: 9,
+                            mb: 0.5
+                        }}>
+                        {cartItemsCount}
+                    </Badge>
+                    :
+                    null
+            }
+            <Pressable
+                onPress={handlePressGoToCart}>
+                <Ionicons
+                    name="cart-outline"
+                    size={25}
+                    color='#fff'
+                />
+            </Pressable>
+        </Box>
+    )
+}
+
+export default CartSection
