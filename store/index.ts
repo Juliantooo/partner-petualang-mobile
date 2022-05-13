941bf998e97ac50f070d95a07dc38a7401d77b21
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-13 16:47:45 +0700

    add store

---
+++ store/index.ts
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit'
+import sellItemsSlice from './slicers/sellItems'
+import userSlice from './slicers/user'
+import whishlistItemsSlice from './slicers/whishlist'
+import cartItemsSlice from './slicers/cartItems'
+import orderItemsSlice from './slicers/orderItems'
+
+export const store = configureStore({
+    reducer: {
+        user: userSlice,
+        sellItems: sellItemsSlice,
+        wishlistItem: whishlistItemsSlice,
+        cartItems: cartItemsSlice,
+        orderItems: orderItemsSlice
+    },
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
