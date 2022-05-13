149829a1fee87a6f47411d6a451adf426f4f5353
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-13 16:16:04 +0700

    fix firebase import and add redux

--- App.tsx
+++ App.tsx
@@ -5,26 +5,29 @@ import { NativeBaseProvider } from "native-base";
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import { useEffect } from 'react';
-import { app } from './firebase/firebase';
+import './firebase/firebase';
+import { LogBox } from 'react-native';
+import { Provider } from 'react-redux'
+import { store } from './store';
+
+
+LogBox.ignoreLogs(['Setting a timer for a long period of time'])
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
-  useEffect(() => {
-    console.log(app)
-  })
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <NativeBaseProvider>
-          <Navigation colorScheme={colorScheme} />
-          <StatusBar />
-        </NativeBaseProvider>
+        <Provider store={store}>
+          <NativeBaseProvider>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+          </NativeBaseProvider>
+        </Provider>
       </SafeAreaProvider>
     );
   }
