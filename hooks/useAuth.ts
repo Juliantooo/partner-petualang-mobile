b926f1eadf6993e6d4310646d84900a9af00dd1f
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-13 16:28:42 +0700

    add useAuth hooks

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,5 +1,7 @@
 import { useState } from "react"
+import { useSelector } from "react-redux"
 import { IAuthValues } from "../libs/interfaces"
+import { RootState } from "../store"
 
 const useAuth = () => {
     const [authValues, setAuthValues] = useState<IAuthValues>({
@@ -7,9 +9,17 @@ const useAuth = () => {
         password: ''
     })
 
+    const isLogin = useSelector((state: RootState) => state.user.authToken);
+    const userData = useSelector((state: RootState) => state.user.userData);
+    const idUser = useSelector((state: RootState) => state.user.userData.id);
+
     return {
         authValues,
-        setAuthValues
+        isLogin,
+        userData,
+        idUser,
+
+        setAuthValues,
     }
 }
 
