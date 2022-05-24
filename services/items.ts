2f8c16e7d3bcf9fd6537d046a60043ccb9c6a1e9
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-24 19:11:22 +0700

    update interface

--- services/items.ts
+++ services/items.ts
@@ -1,11 +1,12 @@
 import { collection, getDocs } from "firebase/firestore"
 import { db } from "../firebase/firebase"
+import { IItem } from "../libs/interfaces";
 
 
 
 export const getItems = async () => {
     const querySnapshot = await getDocs(collection(db, 'items'));
-    const items = []
+    const items: Array<IItem> = []
     querySnapshot.forEach((doc) => {
         items.push({
             ...doc.data(),
