88a43f35cfd8ad8991558184eae0c25ab9c3d573
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-24 19:06:31 +0700

    add method to post to firebase for wishlist items

--- hooks/useWishlistItems.ts
+++ hooks/useWishlistItems.ts
@@ -2,24 +2,31 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import notification from '../libs/notification';
+import { updateWishlistUser } from '../services/user';
 import { RootState } from "../store"
 import { ADD_WISH_LIST_ITEM, REMOVE_WISH_LIST_ITEM } from '../store/slicers/whishlist';
+import useAuth from './useAuth';
 
 const useWishlistItems = () => {
 
     const dispatch = useDispatch();
-    const wishlistItems = useSelector((state: RootState) => state.wishlistItem.whishlistItems);
+    const { userData } = useAuth();
+    const wishlistItems = useSelector((state: RootState) => state.wishlistItem.wishlistItems);
 
     const [wishlistItemsCount, setWishlistItemsCount] = useState<number>(0);
 
-    const addItemToWishlist = (item: any) => {
-        notification.success(`Berhasil menambahkan ke wishlist`);
+    const addItemToWishlist = async (item: any) => {
         dispatch(ADD_WISH_LIST_ITEM(item));
+        notification.success(`Berhasil menambahkan ke wishlist`);
+        const items = [...wishlistItems, item]
+        await updateWishlistUser(items, userData.id);
     }
 
-    const removeItemFromWishlist = (id: string) => {
+    const removeItemFromWishlist = async (id: string) => {
         dispatch(REMOVE_WISH_LIST_ITEM(id));
         notification.success(`Berhasil menghapus wishlist`);
+        const items = wishlistItems.filter((item) => item.id !== id);
+        await updateWishlistUser(items, userData.id);
     }
 
     const isWishlistItem = (id: string) => {
