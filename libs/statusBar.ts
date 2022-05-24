3b49dee05a947c3da05cbca06134b7f0f1932d4f
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-24 19:07:13 +0700

    separate statusbar options

---
+++ libs/statusBar.ts
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface IStackScreenStatusBarOptionsProps {
+    title?: string;
+    headerTintColor?: string;
+    headerShadowVisible?: boolean;
+    headerBackgroundColor?: string;
+    headerTtitleDisplay?: string;
+    headerRight?: any
+}
+
+
+export const StackScreenStatusBarOptions = ({
+    title = '',
+    headerTintColor = '#fff',
+    headerShadowVisible = false,
+    headerBackgroundColor = '#10b981',
+    headerTtitleDisplay = 'none',
+    headerRight = () => { },
+}): IStackScreenStatusBarOptionsProps => {
+    return {
+        title,
+        headerTintColor,
+        headerShadowVisible,
+        headerStyle: {
+            backgroundColor: headerBackgroundColor,
+        },
+        headerTitleStyle: {
+            display: headerTtitleDisplay,
+        },
+        headerRight,
+    }
+}
