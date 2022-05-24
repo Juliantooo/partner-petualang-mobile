93dda92b745261f4b1cdafbecfb5e43513727f21
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-24 19:06:01 +0700

    add orders history, post to firebase, update types

--- hooks/useOrderItems.ts
+++ hooks/useOrderItems.ts
@@ -1,22 +1,44 @@
 import { useDispatch, useSelector } from "react-redux"
+import { IOrder, IOrderItem } from "../libs/interfaces";
 import notification from "../libs/notification";
 import { RootState } from "../store";
-import { SET_ORDER_ITEMS } from "../store/slicers/orderItems";
+import { SET_ORDER, SET_ORDER_HISTORY, SET_ORDER_ITEMS } from "../store/slicers/orderItems";
 
+interface ISetOrderItemsProps {
+    items: IOrderItem[],
+    isShowNotification: boolean
+}
 
 const useOrderItems = () => {
 
     const dispatch = useDispatch();
+    const order = useSelector((state: RootState) => state.orderItems.order);
     const orderItems = useSelector((state: RootState) => state.orderItems.orderItems);
+    const ordersHistory = useSelector((state: RootState) => state.orderItems.ordersHistory);
 
-    const setOrderItems = (items: any) => {
+    const setOrderItems = ({ items, isShowNotification = true }: ISetOrderItemsProps) => {
         dispatch(SET_ORDER_ITEMS(items));
-        notification.success('Lanjutkan pembayaran untuk menyelesaikan transaksi');
+        if (isShowNotification) {
+            notification.success('Lanjutkan pembayaran untuk menyelesaikan transaksi');
+        }
+    }
+
+    const setOrder = (order: IOrder) => {
+        dispatch(SET_ORDER(order));
+    }
+
+    const setOrdersHistory = (orders: IOrder[]) => {
+        dispatch(SET_ORDER_HISTORY(orders));
     }
 
     return {
         orderItems,
-        setOrderItems
+        order,
+        ordersHistory,
+
+        setOrderItems,
+        setOrder,
+        setOrdersHistory
     }
 }
 
