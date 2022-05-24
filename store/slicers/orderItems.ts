5c56fc98d43f4f121c080120081d68d082fc26af
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-24 19:13:55 +0700

    add more reducer

--- store/slicers/orderItems.ts
+++ store/slicers/orderItems.ts
@@ -1,26 +1,48 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { IOrder, IOrderItem } from '../../libs/interfaces'
 
+interface IOrderSliceState {
+    orderItems: Array<IOrderItem>,
+    order: IOrder,
+    ordersHistory: Array<IOrder>
+}
 
-const initialState = {
+const initialState: IOrderSliceState = {
     orderItems: [],
+    order: {},
+    ordersHistory: [],
 }
 
 export const orderItemsSlice = createSlice({
     name: 'orderItems',
     initialState,
     reducers: {
-        SET_ORDER_ITEMS: (state, action: PayloadAction<any>) => {
+        SET_ORDER_ITEMS: (state, action: PayloadAction<IOrderItem[]>) => {
             return {
                 ...state,
                 orderItems: action.payload
             }
-        }
+        },
+        SET_ORDER: (state, action: PayloadAction<IOrder>) => {
+            return {
+                ...state,
+                order: action.payload
+            }
+        },
+        SET_ORDER_HISTORY: (state, action: PayloadAction<IOrder[]>) => {
+            return {
+                ...state,
+                ordersHistory: action.payload
+            }
+        },
     },
 })
 
 // Action creators are generated for each case reducer function
 export const {
-    SET_ORDER_ITEMS
+    SET_ORDER_ITEMS,
+    SET_ORDER,
+    SET_ORDER_HISTORY
 } = orderItemsSlice.actions
 
 export default orderItemsSlice.reducer
