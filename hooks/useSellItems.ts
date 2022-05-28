5d33c1f26a9805a9e2f720775b020df301f24a2c
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-28 16:36:05 +0700

    update use sell items hooks add filter and keyword search

--- hooks/useSellItems.ts
+++ hooks/useSellItems.ts
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { IItem } from "../libs/interfaces";
 import { getItems } from "../services/items";
+import { RootState } from "../store";
 import { SET_SELL_ITEMS } from "../store/slicers/sellItems";
 import useLoading from "./useLoading"
 
 
+interface IfilterItemsProps {
+    sortBy?: string,
+    searchKeywords?: string,
+    items?: IItem[]
+}
+
 const useSellItems = () => {
+    const ASCENDING = 'ASCENDING'
+    const DESCENDING = 'DESCENDING'
 
     const dispatch = useDispatch()
-
+    const searchKeywords = useSelector((state: RootState) => state.sellItems.searchKeywords)
     const { isLoading, setIsLoading } = useLoading();
-    const [items, setItems] = useState<any>([]);
+    const [items, setItems] = useState<IItem[]>([]);
+    const [filteredItems, setFilteredItems] = useState<IItem[]>([]);
 
     const getAllItems = async () => {
         setIsLoading(true)
@@ -18,7 +29,21 @@ const useSellItems = () => {
 
         dispatch(SET_SELL_ITEMS(response));
         setItems(response)
+        filterItems({ sortBy: '', items: response });
         setIsLoading(false)
+    }
+
+    const filterItems = ({ sortBy, items }: IfilterItemsProps) => {
+        if (sortBy === ASCENDING) {
+            const newItems = [...items!].sort((itemA: IItem, itemB: IItem) => itemA.price! - itemB.price!);
+            setFilteredItems(newItems)
+        } else if (sortBy === DESCENDING) {
+            const newItems = [...items!].sort((itemA: IItem, itemB: IItem) => itemB.price! - itemA.price!);
+            setFilteredItems(newItems)
+        } else {
+            setFilteredItems(items!);
+        }
+
     }
 
     useEffect(() => {
@@ -27,11 +52,18 @@ const useSellItems = () => {
 
 
     return {
+        ASCENDING,
+        DESCENDING,
+
         isLoading,
         items,
+        filteredItems,
+        searchKeywords,
 
         setIsLoading,
-        setItems
+        setFilteredItems,
+        setItems,
+        filterItems,
     }
 }
 
