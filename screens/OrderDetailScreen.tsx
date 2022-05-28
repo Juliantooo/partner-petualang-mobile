b7d10e334d4867dbcab7990e265670bb3b344023
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-28 16:39:57 +0700

    update order detail screen

--- screens/OrderDetailScreen.tsx
+++ screens/OrderDetailScreen.tsx
@@ -1,65 +1,113 @@
-import { Box, ScrollView, Text, VStack } from "native-base";
+import { Box, HStack, ScrollView, Text, VStack } from "native-base";
+import { useEffect, useState } from "react";
 import useOrderItems from "../hooks/useOrderItems";
+import { IOrder } from "../libs/interfaces";
 import { RootStackScreenProps } from "../types";
 
 
-export default function OrderDetailScreen({ navigation }: RootStackScreenProps<'OrderDetail'>) {
+export default function OrderDetailScreen({ navigation, route }: RootStackScreenProps<'OrderDetail'>) {
 
-    const { order } = useOrderItems();
+    const { idOrder } = route.params
+    const { ordersHistory } = useOrderItems();
+
+    const [detailOrder, setDetailOrder] = useState<IOrder>({});
+
+    useEffect(() => {
+        const order: IOrder = ordersHistory.find((order: IOrder) => order.id === idOrder);
+        setDetailOrder(order)
+    }, [])
+
+
     return (
         <Box flex='1' backgroundColor='blueGray.50' p='4'>
-            <ScrollView>
-                <VStack space='2'>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Nama</Text>
-                        <Text>{order.user.name}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Email</Text>
-                        <Text>{order.user.email}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>No Hp</Text>
-                        <Text>{order.user.phone}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>No Hp</Text>
-                        <Text>{order.user.phone}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Alamat</Text>
-                        <Text>{order.user.address}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Barang</Text>
-                        {
-                            order.items.map((item) => (
-                                <Text key={item.id} fontWeight='semibold'>- {item.name}</Text>
-                            ))
-                        }
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Pengiriman</Text>
-                        <Text>{order.deliveryMethod}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Diskon</Text>
-                        <Text>Rp {order.discountAmount}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Total Pembayaran</Text>
-                        <Text>Rp {order.totalPayment}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Metode Pembayaran</Text>
-                        <Text>{order.paymentMethod}</Text>
-                    </Box>
-                    <Box rounded='sm' p='2' backgroundColor='white'>
-                        <Text fontWeight='bold'>Status</Text>
-                        <Text color='blue.300' fontWeight='bold'>{order.status}</Text>
-                    </Box>
-                </VStack>
-            </ScrollView>
+            {
+                detailOrder ?
+                    <ScrollView>
+                        <VStack space='2'>
+                            <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                <Text fontWeight='bold'>Waktu transaksi</Text>
+                                <Text>{detailOrder?.orderDate}</Text>
+                            </Box>
+                            <HStack justifyContent='space-between' space='2'>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>Nama</Text>
+                                    <Text>{detailOrder?.user?.name}</Text>
+                                </Box>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>Email</Text>
+                                    <Text>{detailOrder?.user?.email}</Text>
+                                </Box>
+                            </HStack>
+                            <HStack justifyContent='space-between' space='2'>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>No Hp</Text>
+                                    <Text>{detailOrder?.user?.phone}</Text>
+                                </Box>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>Pengiriman</Text>
+                                    <Text>{detailOrder?.deliveryMethod}</Text>
+                                </Box>
+                            </HStack>
+                            <HStack justifyContent='space-between' space='2'>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>Tanggal Sewa</Text>
+                                    <Text>{detailOrder?.startRentDate}</Text>
+                                </Box>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>Tanggal Pengembalian</Text>
+                                    <Text>{detailOrder?.endRentDate}</Text>
+                                </Box>
+                            </HStack>
+                            <HStack justifyContent='space-between' space='2'>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>{`Lama Sewa (Hari)`}</Text>
+                                    <Text>{detailOrder?.daysOfRent}</Text>
+                                </Box>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>Diskon</Text>
+                                    <Text>{detailOrder?.discountAmount}</Text>
+                                </Box>
+                            </HStack>
+                            <HStack justifyContent='space-between' space='2'>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>Total Pembayaran</Text>
+                                    <Text>Rp {detailOrder?.totalPayment}</Text>
+                                </Box>
+                                <Box flex='1' rounded='sm' p='2' backgroundColor='white'>
+                                    <Text fontWeight='bold'>Metode Pembayaran</Text>
+                                    <Text>{detailOrder?.paymentMethod}</Text>
+                                </Box>
+                            </HStack>
+                            <Box rounded='sm' p='2' backgroundColor='white'>
+                                <Text fontWeight='bold'>Barang</Text>
+                                {
+                                    detailOrder?.items?.map((item) => (
+                                        <Box key={item.id} >
+                                            <Text >- {`${item.count} ${item.name}`}</Text>
+                                            {
+                                                item.note ?
+                                                    <Text fontSize='xs'>{`Catatan : ${item.note}`}</Text>
+                                                    :
+                                                    null
+                                            }
+                                        </Box>
+                                    ))
+                                }
+                            </Box>
+
+                            <Box rounded='sm' p='2' backgroundColor='white'>
+                                <Text fontWeight='bold'>Alamat</Text>
+                                <Text>{detailOrder?.user?.address}</Text>
+                            </Box>
+                            <Box rounded='sm' p='2' backgroundColor='white'>
+                                <Text fontWeight='bold'>Status</Text>
+                                <Text color='blue.300' fontWeight='bold'>{detailOrder?.status}</Text>
+                            </Box>
+                        </VStack>
+                    </ScrollView>
+                    :
+                    <Text>Loading</Text>
+            }
         </Box>
     )
 }
