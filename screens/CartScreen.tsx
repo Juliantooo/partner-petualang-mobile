258d9e6dc104b4c09c2b64552d5c79f7a1543939
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-28 16:37:00 +0700

    add input date for rent duration

--- screens/CartScreen.tsx
+++ screens/CartScreen.tsx
@@ -1,13 +1,16 @@
-import { Box, HStack, ScrollView, Text, VStack, Image, Button, Checkbox, Pressable } from "native-base";
+import { Box, HStack, ScrollView, Text, VStack, Image, Button, Checkbox, Pressable, useDisclose } from "native-base";
 import { RootStackScreenProps } from "../types";
 import useCartItems from "../hooks/useCartItems";
 import CartItemCard from "../components/CartItemCard";
-
+import DatePicker from 'react-native-datepicker';
 import EmptyCart from '../assets/images/empty-cart.png';
 import useOrderItems from "../hooks/useOrderItems";
 import { ICartItem } from "../libs/interfaces";
+import { useState } from "react";
+import dayjs from 'dayjs'
+import { StyleSheet } from "react-native";
 
-
+const TODAY = dayjs().format('DD-MM-YYYY');
 export default function CartScreen({ navigation }: RootStackScreenProps<'Cart'>) {
 
     const {
@@ -16,6 +19,7 @@ export default function CartScreen({ navigation }: RootStackScreenProps<'Cart'>)
         itemsCount,
         cartItemsTotalPayment,
 
+        setCartItems,
         subtractCartItemCount,
         addCartItemCount,
         removeItemFromCart,
@@ -27,7 +31,7 @@ export default function CartScreen({ navigation }: RootStackScreenProps<'Cart'>)
         isSelectedItemForOrderContainThisItem
     } = useCartItems();
 
-    const { setOrderItems } = useOrderItems();
+    const { setOrderItems, startRentDate, setStartRentDate, setEndRentDate, daysOfRent, endRentDate, setOrder } = useOrderItems();
 
     const handleChangeValue = (isChecked: boolean) => {
         if (isChecked) return selectAllItemsToSelectedItemForOrder();
@@ -35,9 +39,25 @@ export default function CartScreen({ navigation }: RootStackScreenProps<'Cart'>)
     }
 
     const handlePressCheckout = () => {
-        setOrderItems({ items: selectedItemsForOrder });
+        setOrderItems({ items: selectedItemsForOrder, isShowNotification: false });
+        const order = {
+            startRentDate,
+            endRentDate,
+            daysOfRent,
+        }
+        setOrder(order);
         navigation.navigate('Checkout');
     }
+
+    const handleSetNote = (note: string, idItem: string) => {
+        const item = cartItems.find((cartItem: ICartItem) => cartItem.id === idItem);
+        const idxItem = cartItems.findIndex((cartItem: ICartItem) => cartItem.id === idItem);
+        const newCartItem = { ...item, note }
+        const newCartItems: ICartItem[] = [...cartItems]
+        newCartItems.splice(idxItem, 1, newCartItem)
+        setCartItems(newCartItems)
+    }
+
 
     return (
         <Box flex='1' background='white'>
@@ -65,7 +85,7 @@ export default function CartScreen({ navigation }: RootStackScreenProps<'Cart'>)
                                 <Text fontSize='md' fontWeight='bold' color='tertiary.500'>Hapus</Text>
                             </Pressable>
                         </HStack>
-                        <ScrollView key={selectedItemsForOrder} mb='20'>
+                        <ScrollView key={selectedItemsForOrder} mb='32'>
                             <VStack>
                                 {
                                     cartItems.map((item: ICartItem) => (
@@ -78,6 +98,7 @@ export default function CartScreen({ navigation }: RootStackScreenProps<'Cart'>)
                                             discount={item.discount}
                                             count={item.count}
                                             stock={item.stock}
+                                            note={item.note}
                                             handleAddCartItemCount={addCartItemCount}
                                             handleSubtractCartItemCount={subtractCartItemCount}
                                             handleRemoveItemInCart={removeItemFromCart}
@@ -85,22 +106,88 @@ export default function CartScreen({ navigation }: RootStackScreenProps<'Cart'>)
                                             handleAddItemToSelectedItemForOrder={addItemToSelectedItemForOrder}
                                             handleRemoveItemFromSelectedItemForOrder={removeItemFromSelectedItemForOrder}
                                             isSelectedItemForOrderContainThisItem={isSelectedItemForOrderContainThisItem}
+                                            handleSetNote={handleSetNote}
                                         />
                                     ))
                                 }
                             </VStack>
                         </ScrollView>
-                        <Box w='full' shadow='9' roundedTop='lg' px='4' py='2' h='16' backgroundColor='white' position='absolute' bottom='0'>
-                            <HStack space='3' alignItems='center' justifyContent='space-between'>
+                        <VStack w='full' shadow='9' roundedTop='lg' px='4' py='2' backgroundColor='white' position='absolute' bottom='0'>
+                            <HStack py='1' space='5' alignItems='center' justifyContent='space-between'>
+                                <DatePicker
+                                    mode="date" //The enum of date, datetime and time
+                                    placeholder="Tanggal Pinjam"
+                                    format="DD-MM-YYYY"
+                                    date={startRentDate}
+                                    minDate={TODAY}
+                                    confirmBtnText="Confirm"
+                                    cancelBtnText="Cancel"
+                                    showIcon={false}
+                                    customStyles={{
+                                        dateInput: {
+                                            borderWidth: 0,
+                                        },
+                                        dateText: {
+                                            color: '#fff'
+                                        },
+                                        placeholderText: {
+                                            color: '#fff'
+                                        },
+                                        dateTouchBody: {
+                                            backgroundColor: '#10b981',
+                                            borderRadius: 5,
+                                        }
+                                    }}
+                                    onDateChange={(date) => {
+                                        setStartRentDate(date);
+                                    }}
+                                />
+                                <Text fontSize='lg' fontWeight='bold'>
+                                    {`${daysOfRent} Hari`}
+                                </Text>
+                                <DatePicker
+                                    mode="date" //The enum of date, datetime and time
+                                    placeholder="Tanggal Kembali"
+                                    format="DD-MM-YYYY"
+                                    date={endRentDate}
+                                    disabled={!startRentDate}
+                                    minDate={startRentDate}
+                                    confirmBtnText="Confirm"
+                                    cancelBtnText="Cancel"
+                                    showIcon={false}
+                                    customStyles={{
+                                        dateInput: {
+                                            borderWidth: 0,
+                                        },
+                                        dateText: {
+                                            color: '#fff'
+                                        },
+                                        placeholderText: {
+                                            color: '#fff'
+                                        },
+                                        dateTouchBody: {
+                                            backgroundColor: '#10b981',
+                                            borderRadius: 5,
+                                        },
+                                        disabled: {
+                                            borderRadius: 5,
+                                        }
+                                    }}
+                                    onDateChange={(date) => {
+                                        setEndRentDate(date);
+                                    }}
+                                />
+                            </HStack>
+                            <HStack py='1' space='3' alignItems='center' justifyContent='space-between'>
                                 <VStack space='1'>
                                     <Text fontSize='sm'>Total harga</Text>
-                                    <Text fontSize='lg' lineHeight='xs' fontWeight='bold'>{`Rp ${cartItemsTotalPayment}`}</Text>
+                                    <Text fontSize='lg' lineHeight='xs' fontWeight='bold'>{`Rp ${cartItemsTotalPayment * daysOfRent}`}</Text>
                                 </VStack>
-                                <Button onPress={handlePressCheckout} isDisabled={itemsCount <= 0} variant='solid' colorScheme='tertiary' _text={{ color: '#fff' }} rounded='lg' py='3' px='8'>
+                                <Button onPress={handlePressCheckout} isDisabled={itemsCount <= 0 || daysOfRent === 0} variant='solid' colorScheme='tertiary' _text={{ color: '#fff' }} rounded='lg' py='3' px='8'>
                                     {`Sewa  ( ${itemsCount} )`}
                                 </Button>
                             </HStack>
-                        </Box>
+                        </VStack>
                     </Box>
             }
         </Box>
