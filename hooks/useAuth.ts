e81168973d7f06c80e5d5b2def6ec9b1d94c64cc
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-28 16:34:00 +0700

    fix interfaces

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useSelector } from "react-redux"
-import { IAuthValues } from "../libs/interfaces"
+import { IAuthValues, IUser } from "../libs/interfaces"
 import { RootState } from "../store"
 
 const useAuth = () => {
@@ -9,9 +9,9 @@ const useAuth = () => {
         password: ''
     })
 
-    const isLogin = useSelector((state: RootState) => state.user.authToken);
-    const userData = useSelector((state: RootState) => state.user.userData);
-    const idUser = useSelector((state: RootState) => state.user.userData.id);
+    const isLogin: boolean = useSelector((state: RootState) => state.user.authToken ? true : false);
+    const userData: IUser = useSelector((state: RootState) => state.user.userData);
+    const idUser: string = useSelector((state: RootState) => state.user.userData.id);
 
     return {
         authValues,
