9e1c8d8e3f62bbabdfbd2cb524bcc2cd768aa1e6
Fajar Julianto <slashcrackdie@gmail.com>
2022-05-28 16:41:46 +0700

    add search keyword state

--- store/slicers/sellItems.ts
+++ store/slicers/sellItems.ts
@@ -2,11 +2,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { IItem } from '../../libs/interfaces'
 
 interface IInitialState {
-    sellItems: Array<IItem>
+    sellItems: Array<IItem>,
+    searchKeywords?: string
 }
 
 const initialState: IInitialState = {
     sellItems: [],
+    searchKeywords: ''
 }
 
 export const sellItemsSlice = createSlice({
@@ -18,11 +20,17 @@ export const sellItemsSlice = createSlice({
                 ...state,
                 sellItems: action.payload
             }
-        }
+        },
+        SET_SEARCH_KEYWORDS: (state, action: PayloadAction<string>) => {
+            return {
+                ...state,
+                searchKeywords: action.payload
+            }
+        },
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { SET_SELL_ITEMS } = sellItemsSlice.actions
+export const { SET_SELL_ITEMS, SET_SEARCH_KEYWORDS } = sellItemsSlice.actions
 
 export default sellItemsSlice.reducer
